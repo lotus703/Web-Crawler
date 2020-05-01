5a826d5a8e781bf076033753f1195265b1565d81
lotus703 <mr.hnqtin@gmail.com>
2020-05-01 20:35:59 +0700

    input link want to crawl

---
+++ go.mod
@@ -0,0 +1,8 @@
+module Web-Crawler
+
+go 1.13
+
+require (
+	github.com/PuerkitoBio/goquery v1.5.1 // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
+)
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+type Ebook struct{
+	URL string `json:"url"`
+	Title string `json:"title"`
+	Image string `json:"image"`
+}
+type Ebooks struct{
+	TotalPages int `json:"total_pages"`
+	TotalEbooks int `json:"total_books"`
+	List []Ebook `json:"ebooks"`
+}
+func NewBooks() *Ebooks {
+	return  &Ebooks{}
+}
+func checkError(err error){
+	if err != nil {
+		log.Println(err)
+	}
+}
+func main()  {
+	flag.Parse()
+	args:= flag.Args()
+	if len(args) < 1{
+	fmt.Println("Please specify start page")
+	os.Exit(1)
+	}
+	currentUrl := args[0] // đây là biến lấy ra URL muốn crawl data
+	fmt.Println(currentUrl)
+}
