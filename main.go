7513004cc87bf0502e51f440c8bbff5be751ef3f
lotus703 <mr.hnqtin@gmail.com>
2020-08-05 14:26:54 +0700

    abc

--- main.go
+++ main.go
@@ -6,30 +6,33 @@ import (
 	"log"
 	"os"
 )
-type Ebook struct{
-	URL string `json:"url"`
+
+type Ebook struct {
+	URL   string `json:"url"`
 	Title string `json:"title"`
 	Image string `json:"image"`
 }
-type Ebooks struct{
-	TotalPages int `json:"total_pages"`
-	TotalEbooks int `json:"total_books"`
-	List []Ebook `json:"ebooks"`
+type Ebooks struct {
+	TotalPages  int     `json:"total_pages"`
+	TotalEbooks int     `json:"total_books"`
+	List        []Ebook `json:"ebooks"`
 }
+
 func NewBooks() *Ebooks {
-	return  &Ebooks{}
+	return &Ebooks{}
 }
-func checkError(err error){
+func checkError(err error) {
 	if err != nil {
 		log.Println(err)
 	}
 }
-func main()  {
+func main() {
 	flag.Parse()
-	args:= flag.Args()
-	if len(args) < 1{
-	fmt.Println("Please specify start page")
-	os.Exit(1)
+	args := flag.Args()
+	if len(args) < 1 {
+		fmt.Println("Please specify start page")
+		fmt.Println("Ok!")
+		os.Exit(1)
 	}
 	currentUrl := args[0] // đây là biến lấy ra URL muốn crawl data
 	fmt.Println(currentUrl)
